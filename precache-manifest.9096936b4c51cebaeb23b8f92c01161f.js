096ea04236750cf4b1e6056570e0d579879e88d6
yangyimin <yimin.yang@ifchange.com>
2020-09-08 16:46:43 +0800

    'yxm_msui'

---
+++ precache-manifest.9096936b4c51cebaeb23b8f92c01161f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad39352a4d5971a3161eedbf25c326f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d8915846a2218af33936.bundle.js"
+  },
+  {
+    "url": "runtime~main.d8915846a2218af33936.bundle.js"
+  },
+  {
+    "revision": "eb3aac5882f83e11d0e9",
+    "url": "static/css/main.bc6b6d25.chunk.css"
+  },
+  {
+    "url": "vendors~main.d8915846a2218af33936.bundle.js"
+  },
+  {
+    "url": "vendors~main.d8915846a2218af33936.bundle.js.LICENSE.txt"
+  }
+]);
